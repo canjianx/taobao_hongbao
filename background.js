0e8246df65432b6924867dae4dc4aa38791451c1
Peng <wrzxzp215@163.com>
2014-01-05 14:11:48 +0800

    do work more automatically

--- background.js
+++ background.js
@@ -2,7 +2,4 @@
 var chrome;
 chrome.browserAction.onClicked.addListener(function (tab) {
     console.log("Turning" + tab.url + "red!");
-    chrome.tabs.executeScript({
-        code : "document.body.style.backgroundColor='red'"
-    });
 });
--- qiang.js
+++ qiang.js
@@ -1,16 +1,25 @@
 /*jslint nomen: true, debug: true, evil: true, vars: true, browser:true, devel:true, indent:4 */
 // jinshankuaipan sign-in code.
 var chrome;
+var start = false;
 var qiang = function () {
-    var a = setInterval(function () {
-        //$("#J_GameLotteryReplayBtn").simulate("click")
+    setTimeout(function () {
         $("#J_startgame:visible").simulate("click");
-        $("#J_GameStartBtn:visible").simulate("click");
+    }, 500);
+    var b = setInterval(function () {
+        if ($("#J_GameStartBtn:visible").length !== 0 && !start) {
+            setTimeout(function () {
+                $("#J_GameStartBtn:visible").simulate("click");
+            }, 2000);
+            start = true;
+            clearInterval(b);
+        }
+    }, 1000);
+    var a = setInterval(function () {
         $("#J_GameLotteryReplayBtn:visible").simulate("click");
         $("#quarkasMonster").simulate("mousedown", {"clientX": 692, "clientY": 270});
         $("#quarkasMonster").simulate("mousedown", {"clientX": 792, "clientY": 270});
         $("#quarkasMonster").simulate("mousedown", {"clientX": 892, "clientY": 270});
-        $("#J_GameLotterySureBtn:visible").simulate("click");
     }, 300);
 };
 
