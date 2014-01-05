036963a706297fb3c7df3e5e39080583dbe1cedd
Peng <wrzxzp215@163.com>
2014-01-05 10:18:18 +0800

    update the function of qiang

--- qiang.js
+++ qiang.js
@@ -4,6 +4,8 @@ var chrome;
 var qiang = function () {
     var a = setInterval(function () {
         //$("#J_GameLotteryReplayBtn").simulate("click")
+        $("#J_startgame:visible").simulate("click");
+        $("#J_GameStartBtn:visible").simulate("click");
         $("#J_GameLotteryReplayBtn:visible").simulate("click");
         $("#quarkasMonster").simulate("mousedown", {"clientX": 692, "clientY": 270});
         $("#quarkasMonster").simulate("mousedown", {"clientX": 792, "clientY": 270});
